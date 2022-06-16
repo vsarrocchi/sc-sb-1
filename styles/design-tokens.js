2c6ec4415085133a1d8e08665f2c8cc45b14a6c5
Valesca Sarrocchi <valescasarrocchi@Valescas-MBP.localdomain>
2022-06-16 15:00:36 +0200

    test

--- styles/design-tokens.js
+++ styles/design-tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Thu, 16 Jun 2022 10:08:22 GMT
+ * Generated on Thu, 16 Jun 2022 13:00:06 GMT
  */
 
-export const tokenRed = "#f00";
+export const tokenRed = "#0ff";
