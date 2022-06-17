d5fb25d6758a7b51e3447116ddc1f0abd559ca0c
vsarrocchi <vsarrocchi@users.noreply.github.com>
2022-06-17 09:23:21 +0000

    Update platform deliverables

--- styles/design-tokens.js
+++ styles/design-tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 17 Jun 2022 09:02:42 GMT
+ * Generated on Fri, 17 Jun 2022 09:23:21 GMT
  */
 
-export const tokenRed = "#f0f";
+export const tokenRed = "#ffad00";
