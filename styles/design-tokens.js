72075905dc9fdc0c41b474474b256f227e489b7f
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2022-06-19 08:59:54 +0200

    test

--- styles/design-tokens.js
+++ styles/design-tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Sun, 19 Jun 2022 06:04:31 GMT
+ * Generated on Sun, 19 Jun 2022 06:59:34 GMT
  */
 
-export const tokenRed = "#25d3c7";
+export const tokenRed = "#d426d1";
